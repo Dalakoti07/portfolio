5a6d1b110475cf272d5511fe7fcd4343db5c13c0
lonewolf <saurabhdalakoti07@gmail.com>
2020-09-29 00:51:21 +0530

    added the project tagwise functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import NavBarComponent from './components/NavBar'
 import AboutComponent from './components/AboutComponent';
@@ -7,17 +7,24 @@ import ExperienceComponent from './components/ExperienceComponent';
 import ContactComponent from './components/ContactComponent';
 import {getTheProjects,getTheTags,getProjectsByTag} from './utils/DataProvider'
 
+var currentProjects
+var setProjectList
+
 function chipsClicked(tag) {
   let filteredList= getProjectsByTag(tag)
   console.log(filteredList);
+  setProjectList(projects=> filteredList)
 }
 
 function App() {
+  // using react hooks in functional-components, making state and stateSetter global, maybe bad practise
+  [currentProjects,setProjectList]= useState(()=> getTheProjects())
+
   return (
     <div className="App">
       <NavBarComponent/>
       <AboutComponent/>
-      <ProjectComponent projectsList={getTheProjects()}  tags={getTheTags()} tagOnClick={chipsClicked}/>
+      <ProjectComponent projectsList={currentProjects}  tags={getTheTags()} tagOnClick={chipsClicked}/>
       <ExperienceComponent/>
       <ContactComponent/>
     </div>
