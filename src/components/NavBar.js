0867a1892be709db4b8c8e77cd01cce3097625fc
lonewolf <saurabhdalakoti07@gmail.com>
2020-09-29 02:02:41 +0530

    nav-bar corrected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
 
+
 function NavBarComponent(){
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-                <Nav.Link href="#project">Project</Nav.Link>
-                <Nav.Link href="#experience">Experience</Nav.Link>
-                <Nav.Link href="#contests">Coding-Contests</Nav.Link>
+            <Nav.Link href="#project">Project</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#contests">Coding-Contests</Nav.Link>
             </Nav>
+        </Navbar.Collapse>
         </Navbar>
     )
 }
